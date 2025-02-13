88647efc1075f923fae93db6c69ce183cbbd0a84
blinminipekka <166027067+blinminipekka@users.noreply.github.com>
2025-02-13 16:20:48 +0100

    sadawdawd

--- moduli6/index.php
+++ moduli6/index.php
@@ -6,18 +6,55 @@ $employees = array(
     array(
         "name" => "nil",
         "age" => "15",
-        "position" => "janitor",
-    )
+        "position" => "janitor"
+    ),
     array(
         "name" => "anik",
         "age" => "16",
-        "position" => "employee",
-    )
+        "position" => "employee"
+    ),
     array(
         "name" => "eden",
         "age" => "16",
-        "position" => "ceo",
-    )
+        "position" => "ceo"
+    ),
     );
+
+
+
+
+
+
+
+$products = array(
+    array(
+        "product" => "milk",
+        "cost" => 4
+    ),
+    array(
+        "product" => "eggs",
+        "cost" => 2
+    ),
+    array(
+        "product" => "flour",
+        "cost" => 7
+    ),
+    );
+
+
+
+function calculate($product){
+    $sum = 0;
+    foreach($product as $item){
+
+        
+            $sum += $item["cost"];
+    }
+        return $sum;
+            
+    }
+
+
+echo calculate($products);
 
 ?>
