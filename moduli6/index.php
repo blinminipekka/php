82c7e4fef3cf6ad1e815b031da5010c7d61d2296
blinminipekka <166027067+blinminipekka@users.noreply.github.com>
2025-02-13 15:56:06 +0100

    adasdawd

---
+++ moduli6/index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+$employees = array(
+    array(
+        "name" => "nil",
+        "age" => "15",
+        "position" => "janitor",
+    )
+    array(
+        "name" => "anik",
+        "age" => "16",
+        "position" => "employee",
+    )
+    array(
+        "name" => "eden",
+        "age" => "16",
+        "position" => "ceo",
+    )
+    );
+
+?>
